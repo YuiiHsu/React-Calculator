3669d84beffb48a6bd85d112da6bb41233ef464f
YuiiHsu <54923318+YuiiHsu@users.noreply.github.com>
2022-02-21 16:16:10 +0800

    新增監聽鍵盤輸入

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import style from './App.module.css';
 import { useRef, useState } from 'react';
 
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '00', '.'];
-const operators =
-{
-  into: '÷',
-  times: '×',
-  add: '+',
-  from: '-',
-  equal: '='
-};
-const errorMsg = {
-  Infinity: 'Infinity',
-  Error: 'Error'
-}
-
 function App() {
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '00', '.'];
+  const operators =
+  {
+    into: '÷',
+    times: '×',
+    add: '+',
+    from: '-',
+    equal: '='
+  };
+  const errorMsg = {
+    Infinity: 'Infinity',
+    Error: 'Error'
+  }
+
   // 當前總結果
   const result = useRef(0);
   const isTotal = useRef(true);
@@ -32,7 +32,7 @@ function App() {
    * @param {*} clickedNum 被點擊的數字
    */
   const handleNumberClicked = (e) => {
-    const clickedNum = e.target.innerText;
+    const clickedNum = handleInputSource(e);
     if (isCurrentError() || !clickedNum) return;
     const isContaonDots =
       currentNumber
@@ -50,6 +50,7 @@ function App() {
       reCalculator();
     }
 
+    // 判斷文字是否合法
     const num = currentNumber
       && ((isFirstZero && clickedNum === '.') || !isFirstZero)
       && (!isTotal.current || (isTotal.current && clickedNum === '.'))
@@ -80,13 +81,40 @@ function App() {
     }
   }
 
+  /**
+   * process超過一定長度時，改成xxx ... xxx顯示
+   * @param {*} text 
+   */
+  const settingProcessSize = (text) => {
+    let result = text ? text : '';
+    const length = text.length;
+    if (length > 35) {
+      result = `${text.slice(0, 5)} ... ${text.slice(length - 5, length)}`
+    }
+
+    setProcess(result);
+  }
+
+  /**
+   * 來自鍵盤或滑鼠點擊的動作
+   */
+  const handleInputSource = (input) => {
+    return input.target ? input.target.innerText : input;
+  }
+
+  /**
+   * 點擊AC鍵後，重置計算機
+   */
   const reset = () => {
     settingNumberAndSize('0')
-    setProcess('');
+    settingProcessSize('');
     result.current = 0;
     document.getElementById("result").style.fontSize = '56px';
   }
 
+  /**
+   * 刪除一個字元
+   */
   const remove = () => {
     if (isCurrentError() || !currentNumber) return;
 
@@ -96,10 +124,12 @@ function App() {
     else {
       settingNumberAndSize('0');
     }
-
-    return;
   }
 
+  /**
+   * 確認當前顯示文字是不是錯誤訊息
+   * @returns 當前顯示文字是不是錯誤訊息
+   */
   const isCurrentError = () => {
     return Object.values(errorMsg).includes(currentNumber);
   }
@@ -115,12 +145,16 @@ function App() {
     return inputNum.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   }
 
+  /**
+   * 處理加減乘除及等號
+   * @param {*} e 動作
+   */
   const handleOperatorAction = (e) => {
     if (isCurrentError()) return;
-    const action = e.target.innerText;
+    const action = handleInputSource(e);
     // 改運算符號
     if (process && isTotal.current) {
-      setProcess(`${process.slice(0, -1)} ${action}`);
+      settingProcessSize(`${process.slice(0, -1)} ${action}`);
       operator.current = action;
       return;
     }
@@ -147,7 +181,7 @@ function App() {
         break;
     }
 
-    setProcess(operator.current === operators.equal
+    settingProcessSize(operator.current === operators.equal
       ? `${currentNumber} ${action}`
       : `${process} ${currentNumber} ${action}`);
     operator.current = action;
@@ -158,8 +192,49 @@ function App() {
   // 按了等於後，再重新計算
   const reCalculator = () => {
     result.current = 0;
-    setProcess('');
+    settingProcessSize('');
     operator.current = operators.add;
+  }
+
+  // 監聽鍵盤輸入
+  document.onkeydown = function (event) {
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    if (!e) return;
+    if (e.keyCode === 8) {
+      remove();
+    } else if (e.keyCode === 187 && event.shiftKey) {
+      handleOperatorAction(operators.add);
+    } else if (e.keyCode === 13 || e.keyCode === 187) {
+      handleOperatorAction(operators.equal);
+    } else if (e.keyCode === 189 && event.shiftKey) {
+      handleOperatorAction(operators.from);
+    } else if (e.keyCode === 56 && event.shiftKey) {
+      handleOperatorAction(operators.times);
+      return;
+    } else if (e.keyCode === 191) {
+      handleOperatorAction(operators.into);
+    }
+    else if (e.keyCode === 48) {
+      handleNumberClicked('0')
+    } else if (e.keyCode === 49) {
+      handleNumberClicked('1')
+    } else if (e.keyCode === 50) {
+      handleNumberClicked('2')
+    } else if (e.keyCode === 51) {
+      handleNumberClicked('3')
+    } else if (e.keyCode === 52) {
+      handleNumberClicked('4')
+    } else if (e.keyCode === 53) {
+      handleNumberClicked('5')
+    } else if (e.keyCode === 54) {
+      handleNumberClicked('6')
+    } else if (e.keyCode === 55) {
+      handleNumberClicked('7')
+    } else if (e.keyCode === 56) {
+      handleNumberClicked('8')
+    } else if (e.keyCode === 57) {
+      handleNumberClicked('9')
+    }
   }
 
   return (
@@ -167,7 +242,7 @@ function App() {
       <div className={style.container}>
         {/* 顯示輸入資訊 */}
         <div className={style.inputShow}>
-          <div className={style.process}>
+          <div id="process" className={style.process}>
             {process}
           </div>
           <div id="result" className={style.result}>
@@ -208,8 +283,8 @@ function App() {
           <div className={style.actionBtn} onClick={remove}>
             ⌫
           </div>
-          <div className={style.enterBtn}>
-            <div className={style.text} onClick={handleOperatorAction}>
+          <div className={style.enterBtn} onClick={handleOperatorAction}>
+            <div className={style.text}>
               =
             </div>
           </div>
